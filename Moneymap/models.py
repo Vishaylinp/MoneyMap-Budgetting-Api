40c9de636993cfe08c82cf081296aeab44a3ac2c
Vishaylinp <vishaylinparshotam@gnail.com>
2024-10-04 08:51:27 -0700

    created a user and transaction class for the creation of the database

---
+++ Moneymap/models.py
@@ -0,0 +1,23 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    """Represents a User in the database"""
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+class Transaction(db.Model):
+    """Represents a transaction of the User"""
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(30), nullable=False)
+    Date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user = db.relationship('User', backref=db.backref("Transaction", lazy=True))    
