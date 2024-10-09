b50c07c28bc8255b8e2fbb34a0838bcf3a718c4c
Vishaylinp <vishaylinparshotam@gnail.com>
2024-10-09 09:33:07 -0700

    set up the database Url for SQLALCHEMY to use a SQLite database named money_map.db

--- Moneymap/app.py
+++ Moneymap/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+"""from flask_migrate import Migrate"""
 from Moneymap.models import db
 from Moneymap.routes.authen_routes import authen_bp
 from Moneymap.routes.budget_routes import budget_bp
+from Moneymap.routes.report_routes import report_bp
 
 app = Flask(__name__)
 
@@ -12,9 +14,11 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
+"""migrate = Migrate(app, db)"""
+
 app.register_blueprint(authen_bp)
 app.register_blueprint(budget_bp)
-
+app.register_blueprint(report_bp)
 
 with app.app_context():
     db.create_all()
