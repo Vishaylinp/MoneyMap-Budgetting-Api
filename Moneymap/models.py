5fcfe851d01b8e7f182b29d93a415f2dfc84cc1b
Vishaylinp <vishaylinparshotam@gnail.com>
2024-10-10 08:50:28 -0700

    Add Category for transaction categories

--- Moneymap/models.py
+++ Moneymap/models.py
@@ -11,13 +11,26 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
 
+    transactions = db.relationship('Transaction', backref='user', lazy=True)
+
 class Transaction(db.Model):
     """Represents a transaction of the User"""
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     amount = db.Column(db.Float, nullable=False)
-    category = db.Column(db.String(30), nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow)
 
-    user = db.relationship('User', backref=db.backref("Transaction", lazy=True))    
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    category = db.relationship('Category', backref='transactions', lazy=True)
+
+class Category(db.Model):
+    """Represents a categories of a transaction"""
+    __tablename__ = 'category' 
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(50), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<Category {self.category_name}>'
+
+     
