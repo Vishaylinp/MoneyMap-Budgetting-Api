142814d9e86dc8c18480de62b741cba11065b44a
Vishaylinp <vishaylinparshotam@gnail.com>
2024-10-10 08:53:07 -0700

    Generate JWT access token upn user login

--- Moneymap/routes/authen_routes.py
+++ Moneymap/routes/authen_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 from Moneymap.models import db, User
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import create_access_token, jwt_required 
 
 authen_bp = Blueprint('authen', __name__)
 bcrypt = Bcrypt()
@@ -23,7 +24,7 @@ def register():
 
     except Exception as e:
         db.session.rollback()
-        return jsonify(message="An error occured during registration: " + str(e)), 500
+        return jsonify(message="An error occurred during registration: " + str(e)), 500
 
 @authen_bp.route('/login', methods=['POST'])
 def login():
@@ -31,7 +32,13 @@ def login():
     user = User.query.filter_by(username=data['username']).first()
 
     if user and bcrypt.check_password_hash(user.password, data['password']):
-        return jsonify(message="Login successful"), 200
+        access_token = create_access_token(identity=user.id)
+        return jsonify(message="Login successful", access_token=access_token), 200
 
     else:
         return jsonify(message="Incorrect username or password"), 401
+
+@authen_bp.route('/logout', methods=['POST'])
+@jwt_required()
+def logout():
+    return jsonify(message="Logout successful"), 200
